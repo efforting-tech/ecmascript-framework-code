44cf3ea30c48322ac3c8336f03b46351946826aa
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-04-22 22:28:10 +0200

    Changed order of arguments for assert_equality

--- testing/framework/utils.js
+++ testing/framework/utils.js
@@ -1,4 +1,4 @@
-export function assert_equality(expected, value, message = "Assertion failed, expected ${expected_json} but got ${value_json}") {
+export function assert_equality(value, expected, message = "Assertion failed, expected ${expected_json} but got ${value_json}") {
 	const expected_json = JSON.stringify(expected);
 	const value_json = JSON.stringify(value);
 	if (expected_json !== value_json) {

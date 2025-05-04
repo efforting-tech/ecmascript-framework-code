a35cff2ef018a4bce7fcfa923cd980d4c54fc5e1
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-05-04 19:36:51 +0200

    Tree node iteration and walking is now working properly

--- testing/tests/tt-processing-basic.js
+++ testing/tests/tt-processing-basic.js
@@ -3,11 +3,12 @@ import * as tt_processing from '../../lib/text/tree-processing.js';
 import * as log from '../../lib/debug/console.js';
 
 
-const root = tt_processing.Node.from_string(
+const doc = tt_processing.Node.from_string(
 `	Root
-
+			Stray 1
 		Child 1
 			Grand child 1
+			Grand child 2
 		Child 2
 
 	other-root
@@ -15,49 +16,26 @@ const root = tt_processing.Node.from_string(
 `)
 
 
-assert_equality(root.body.owner.to_text(), '\n\t\tChild 1\n\t\t\tGrand child 1\n\t\tChild 2\n');
-assert_equality([...root.iter_nodes().map(node => node.to_text())], ['\tRoot\n\n\t\tChild 1\n\t\t\tGrand child 1\n\t\tChild 2\n', '\tother-root\n\t\tmore stuff\n']);
 
+const [root, other_root] = doc;
+root.parent = other_root.parent = doc;	// Super root must be set manually
 
-process.exit(2);
+const [stray, ...children] = root.body;
+const [...grand_children] = children[0].body;
 
+assert_equality(root.title, 						'Root');
+assert_equality(other_root.title, 					'other-root');
+assert_equality(stray.title, 						'Stray 1');
+assert_equality(children[0].title, 					'Child 1');
+assert_equality(children[1].title, 					'Child 2');
+assert_equality(grand_children[0].title, 			'Grand child 1');
+assert_equality(grand_children[1].title, 			'Grand child 2');
 
+assert_equality(grand_children[0].parent.title, 	children[0].title);
+assert_equality(grand_children[1].parent.title, 	children[0].title);
 
-const [root_node] = root.iter_nodes();
-log.Debug(root_node.title);	// Root
+assert_equality(grand_children[0].root,				doc);
 
-for (const n2 of root_node.iter_nodes()) {	//This should just yield the same node again (no slicing)
-	log.Debug(n2.owner.to_text());
-}
+assert_equality(children[0].indent_offset,			0);
+assert_equality(stray.indent_offset,				1);
 
-
-
-/*console.log(root_node.title);
-
-for (const n of root_node.iter_nodes()) {
-	console.log(n.title);
-}
-*/
-process.exit(4);
-const children = [...root_node.body.iter_nodes()];
-
-
-
-for (const node of children[1].body.iter_nodes()) {
-	console.log(node.title, node.line_view);
-}
-
-
-
-if (children[0].title !== 'Child 1') {
-	throw new Error('Validation failed')
-}
-
-
-if (children[1].title !== 'Child 2') {
-	throw new Error('Validation failed')
-}
-
-if (children[0].body.iter_nodes().next().value.title !== 'Grand child 1') {
-	throw new Error('Validation failed')
-}

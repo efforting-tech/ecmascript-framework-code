d37e91987d9f0f06536be8fb47ce26330635ec3c
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-05-27 23:00:40 +0200

    Implemented cache map in lib

--- testing/tests/cm-basic.js
+++ testing/tests/cm-basic.js
@@ -1,31 +1,4 @@
-class Cache_Map extends Map {
-	constructor(source=undefined, factory=undefined) {
-		super();
-		this.factory = factory;
-		if (source) {
-			if (source instanceof Map || Array.isArray(source)) {
-				for (const [key, value] of source) {
-					this.set(key, value);
-				}
-			} else {
-				for (const [key, value] of Object.entries(source)) {
-					this.set(key, value);
-				}
-			}
-		}
-	}
-
-	require(key, factory=undefined) {
-		if (this.has(key)) {
-			return this.get(key);
-		} else {
-			const value = (factory ?? this.factory)(key);
-			this.set(key, value);
-			return value;
-		}
-	}
-}
-
+import { Cache_Map } from '../../lib/data/cache.js';
 
 const m = new Map();
 m.set('hello', 'world');

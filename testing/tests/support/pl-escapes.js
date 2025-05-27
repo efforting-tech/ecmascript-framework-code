be9f81d6ee7825254febc9e70bf94032f1ec4745
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-05-27 23:03:17 +0200

    Added a note about further improvement

--- testing/tests/support/pl-escapes.js
+++ testing/tests/support/pl-escapes.js
@@ -10,6 +10,8 @@ const ESCAPE_LUT_VIEWS = {
 //One potential problem here is that we may want these symbols to be defined in a canonical root somewhere. - or perhaps this is where they live!?
 //	This could be the place they live and we may have more tables here for other token types
 
+// To address the above we should utilize Cache_Map like https://github.com/efforting-tech/ecmascript-framework-code/blob/bd1b087b634ef035e1640df84afe0136d82215b2/testing/tests/cm-basic.js#L12
+
 
 class Escape {
 	constructor(escape, symbol, representation, description) {
